883ec4a618dab4e9ff292b4cde6be93efe34d1e8
Maximiliano Pincheira <maximilianopincheira1999@gmail.com>
2020-08-31 12:47:01 -0400

    Segundo commit

--- app/src/main/java/com/pincheira/proyecto_android/MainActivity.java
+++ app/src/main/java/com/pincheira/proyecto_android/MainActivity.java
@@ -2,13 +2,24 @@ package com.pincheira.proyecto_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Creamos la variables que haran referecia a las widgets
+    private TextView txtTitulo;
+    private Button btnIniciar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // realizar las referencias
+        txtTitulo = findViewById(R.id.txtTitulo);
+        btnIniciar = findViewById(R.id.btnIniciar);
+
     }
 }
